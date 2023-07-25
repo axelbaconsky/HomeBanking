5146e745df027f473d2729b2112969e60427d0fc
Axel <Axel@DESKTOP-7H832H0>
2023-07-25 10:41:33 -0300

    Agregar archivos de proyecto.

---
+++ HomeBankingMindHub/Models/DbInitializer.cs
@@ -0,0 +1,39 @@
+﻿using System.Linq;
+
+namespace HomeBankingMindHub.Models
+{
+    public class DBInitializer
+    {
+        public static void Initialize(HomeBankingContext context)
+        {
+            if (!context.Clients.Any())
+            {
+                //Creamos los datos de prueba
+                var clients = new Client[]
+                {
+                    new Client {
+                        FirstName="Eduardo",
+                        LastName="Mendoza",
+                        Email = "eduardo@gmail.com",
+                        Password="123456"
+                },
+                    new Client {
+                    FirstName = "Rafael",
+                    LastName = "Rodriguez",
+                    Email = "rafael@gmail.com",
+                    Password = "123456"
+                },
+             };
+
+            foreach (Client client in clients)
+                {
+                    context.Clients.Add(client);
+                }
+
+                //guardamos
+                context.SaveChanges();
+            }
+
+        }
+    }
+}
